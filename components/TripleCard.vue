fe957e1c4b49c842e73e0a73d65d372423505d05
IamCharlesM <rafroyevans@gmail.com>
2023-02-12 23:39:09 -0600

    update card BG

--- components/TripleCard.vue
+++ components/TripleCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="hero h-96 lg:w-96 lg:rounded-xl"
-    style="background-image: url(_nuxt/assets/roger-back.jpg)"
+    style="background-image: url(../assets/roger-back.jpg)"
   >
     <div class="hero-overlay bg-opacity-60 hover:bg-opacity-20"></div>
     <div class="hero-content text-center text-neutral-content">
